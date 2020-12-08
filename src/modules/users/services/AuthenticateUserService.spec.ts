a750efe1e3750d53a0fe39bd6971de7419dd1ccf
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2020-12-07 23:53:38 -0300

    refatoracao de testes

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -4,14 +4,20 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
+let authenticateUser: AuthenticateUserService;
+
 describe('AuthenticateUser', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeHashProvider = new FakeHashProvider();
+        createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+        authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    });
     it('should be able to authenticate', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeHashProvider = new FakeHashProvider();
-
-        const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
-        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
-
         const user = await createUser.execute({
             name: 'Leonardo',
             email: 'leonardodias96@outlook.com',
@@ -23,16 +29,11 @@ describe('AuthenticateUser', () => {
             password: '123456',
         });
 
-        expect(response).toHaveProperty('token');
-        expect(response.user).toEqual(user);
+        await expect(response).toHaveProperty('token');
+        await expect(response.user).toEqual(user);
     });
 
     it('should not be able to authenticate with nom existing user', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeHashProvider = new FakeHashProvider();
-
-        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
-
         await expect(authenticateUser.execute({
             email: 'leonardodias96@outlook.com',
             password: '123456',
@@ -41,12 +42,6 @@ describe('AuthenticateUser', () => {
     });
 
     it('should not be able to authenticate with wrong password', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeHashProvider = new FakeHashProvider();
-
-        const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
-        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
-
         await createUser.execute({
             name: 'Leonardo',
             email: 'leonardodias96@outlook.com',
--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -4,12 +4,17 @@ import FakeStorageProvider from '@shared/container/providers/StorageProvider/fak
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 
-describe('UpdateUserAvatar', () => {
-    it('should be able to update user avatar', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeStorageProvider = new FakeStorageProvider();
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
 
-        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+describe('UpdateUserAvatar', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeStorageProvider = new FakeStorageProvider();
+        updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+    });
+    it('should be able to update user avatar', async () => {
 
         const user = await fakeUsersRepository.create({
             name: 'Leonardo',
@@ -26,11 +31,6 @@ describe('UpdateUserAvatar', () => {
     });
 
     it('should not be able to update avatar from non existing user', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeStorageProvider = new FakeStorageProvider();
-
-        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
-
         await expect(updateUserAvatar.execute({
             user_id: 'non-existing-user',
             avatarFilename: 'avatar.jpg',
@@ -39,12 +39,7 @@ describe('UpdateUserAvatar', () => {
     });
 
     it('should delete old avatar when updating new one', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeStorageProvider = new FakeStorageProvider();
-
         const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
-
-        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
 
         const user = await fakeUsersRepository.create({
             name: 'Leonardo',
@@ -62,8 +57,8 @@ describe('UpdateUserAvatar', () => {
             avatarFilename: 'avatar2.jpg',
         });
 
-        expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
-        expect(user.avatar).toBe('avatar2.jpg');
+        await expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+        await expect(user.avatar).toBe('avatar2.jpg');
     });
 
 });
