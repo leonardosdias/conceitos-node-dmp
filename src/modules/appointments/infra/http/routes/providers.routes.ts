db9462524abb57ac4e7540980871611ee9e89fb9
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2020-12-17 00:33:29 -0300

    validacao de dados obrigatorios para as rotas http

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,19 +1,38 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
-import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
-const providersRoute = Router();
+const providersRouter = Router();
+
 const providersController = new ProvidersController();
-const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
-providersRoute.use(ensureAuthenticated);
+providersRouter.use(ensureAuthenticated);
 
-providersRoute.get('/', providersController.index);
-providersRoute.get('/:provider_id/dia-disponivel', providerDayAvailabilityController.index);
-providersRoute.get('/:provider_id/mes-disponivel', providerMonthAvailabilityController.index);
+providersRouter.get('/', providersController.index);
 
-export default providersRoute;
+providersRouter.get(
+    '/:provider_id/mes-disponivel',
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
+    providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+    '/:provider_id/dia-disponivel',
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
+    providerDayAvailabilityController.index,
+);
+
+export default providersRouter;
