ee4c2bbeec6fbd90e46940322bd0fd3dff897f3a
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2020-12-17 22:21:22 -0300

    reorganizacao de containers de injecao de dependencias

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -3,4 +3,8 @@ import { container } from 'tsyringe';
 import IStorageProvider from './models/IStorageProvider';
 import DiskStorageProvider from './implementations/DiskStorage';
 
-container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
+const providers = {
+    disk: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>('StorageProvider', providers.disk);
